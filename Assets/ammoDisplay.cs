bb831b61b52eb5f0764904a3dd5fcb0f8cd23f1f
JoshuaWorton <rjr560@york.ac.uk>
2025-11-20 17:52:16 +0000

    ammo

---
+++ Assets/ammoDisplay.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class ammoDisplay : MonoBehaviour
+{
+
+public GameObject ShhotGun;
+public Component AmmoPickup;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        ammoPickup = ShhotGuAn.gameObject
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int ammoCount = 
+    }
+}
